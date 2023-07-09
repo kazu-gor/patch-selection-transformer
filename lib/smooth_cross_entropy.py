46c30f0135eec16138e087d72e5ff50d35b4e403
kazu-gor <ik126@icloud.com>
2023-07-09 17:33:58 +0900

    init

---
+++ lib/smooth_cross_entropy.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SmoothCrossEntropy(nn.Module):
+    def __init__(self, alpha=0.1):
+        super(SmoothCrossEntropy, self).__init__()
+        self.alpha = alpha
+
+    def forward(self, logits, labels):
+        num_classes = logits.shape[-1]
+        alpha_div_k = self.alpha / num_classes
+        target_probs = F.one_hot(labels, num_classes=num_classes).float() * \
+            (1. - self.alpha) + alpha_div_k
+        loss = -(target_probs * torch.log_softmax(logits, dim=-1)).sum(dim=-1)
+        return loss.mean()
---
+++ train.sh
@@ -0,0 +1,1 @@
+PYTHONPATH="./swin-transformer/:$PYTHONPATH" python3 main.py --cfg ./swin-transformer/configs/pst/swin_base_patch4_window12_352_finetune_patch_selection.yaml --local_rank 0 --output output/pst/default
