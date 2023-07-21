302623209b838b9742830f6c364d7d5888078ea8
kazu-gor <ik126@icloud.com>
2023-07-22 03:06:18 +0900

    完成しました

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ import datetime
 import argparse
 import numpy as np
 
+from PIL import Image
+
 import torch
+import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torch.distributed as dist
 
-from timm.utils import accuracy, AverageMeter
+# from timm.utils import accuracy, AverageMeter
+from timm.utils import AverageMeter
 
 from lib.dataloader import get_loader
 from lib.smooth_cross_entropy import SmoothCrossEntropy
@@ -20,7 +24,7 @@ from config import get_config
 from models import build_model
 from logger import create_logger
 from lr_scheduler import build_scheduler
-from utils import save_checkpoint, reduce_tensor, auto_resume_helper
+from utils import save_checkpoint, reduce_tensor, auto_resume_helper, NativeScalerWithGradNormCount
 
 
 def parse_option():
@@ -92,20 +96,23 @@ def main(config, args):
     # define dataloader
     train_loader = get_loader(image_root,
                               gt_root,
-                              shuffle=True,  # FIXME
+                              shuffle=True,
                               batchsize=config.DATA.BATCH_SIZE,
-                              trainsize=config.DATA.IMG_SIZE)
+                              trainsize=config.DATA.IMG_SIZE,
+                              patch_size=config.MODEL.SWIN.PATCH_SIZE)
     val_loader = get_loader(image_root_val,
                             gt_root_val,
                             shuffle=False,
-                            batchsize=config.DATA.BATCH_SIZE,
+                            batchsize=1,
                             trainsize=config.DATA.IMG_SIZE,
+                            patch_size=config.MODEL.SWIN.PATCH_SIZE,
                             phase='val')
     # dataloaders_dict = {"train": train_loader, "val": val_loader}
 
     # define model
     logger.info(f"Creating model:{config.MODEL.TYPE}/{config.MODEL.NAME}")
     model = build_model(config)
+    loss_scaler = NativeScalerWithGradNormCount()
     logger.info(str(model))
 
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -118,8 +125,6 @@ def main(config, args):
     model_without_ddp = model
 
     # define optimizer
-    # TODO: Adam
-    # params = [p for v in model.values() for p in list(v.parameters())]
     params = [p for p in model.parameters()]
     optimizer = torch.optim.Adam(
         [
@@ -141,7 +146,8 @@ def main(config, args):
     #     criterion = LabelSmoothingCrossEntropy(smoothing=config.MODEL.LABEL_SMOOTHING)
     # else:
     #     criterion = torch.nn.CrossEntropyLoss()
-    criterion = SmoothCrossEntropy(alpha=0.1)
+    # criterion = SmoothCrossEntropy(alpha=0.1)
+    criterion = nn.BCEWithLogitsLoss()
 
     max_accuracy = 0.0
 
@@ -174,19 +180,18 @@ def main(config, args):
     #     throughput(data_loader_val, model, logger)
     #     return
 
-    # TODO: train the model
     logger.info("Start training")
     start_time = time.time()
     for epoch in range(config.TRAIN.START_EPOCH, config.TRAIN.EPOCHS):
         train_one_epoch(config, model, criterion, train_loader,
-                        optimizer, epoch, lr_scheduler)
-        if dist.get_rank() == 0 and (epoch % config.SAVE_FREQ == 0 or epoch == (config.TRAIN.EPOCHS - 1)):
+                        optimizer, epoch, lr_scheduler, loss_scaler)
+        if (epoch % config.SAVE_FREQ == 0 or epoch == (config.TRAIN.EPOCHS - 1)):
             save_checkpoint(config, epoch, model_without_ddp, max_accuracy, optimizer, lr_scheduler, loss_scaler, logger)
 
         # test the model
-        acc1, acc5, loss = validate(config, val_loader, model)
-        logger.info(f"Accuracy of the network on the {len(dataset_val)} test images: {acc1:.1f}%")
-        max_accuracy = max(max_accuracy, acc1)
+        acc1, acc2, acc3, loss = validate(config, val_loader, model)
+        logger.info(f"Accuracy of the network on the {len(val_loader)} test images: {acc1:.1f}% {acc2:.1f}% {acc3:.1f}%")
+        max_accuracy = max(max_accuracy, acc1, acc2, acc3)
         logger.info(f'Max accuracy: {max_accuracy:.2f}%')
 
     total_time = time.time() - start_time
@@ -194,26 +199,31 @@ def main(config, args):
     logger.info('Training time {}'.format(total_time_str))
 
 
-def train_one_epoch(config, model, criterion, data_loader, optimizer, epoch, lr_scheduler):
+def train_one_epoch(config, model, criterion, data_loader, optimizer, epoch, lr_scheduler, loss_scaler):
     model.train()
     optimizer.zero_grad()
 
     num_steps = len(data_loader)
     batch_time = AverageMeter()
     loss_meter = AverageMeter()
+    loss1_meter = AverageMeter()
+    loss2_meter = AverageMeter()
+    loss3_meter = AverageMeter()
     scaler_meter = AverageMeter()
 
     start = time.time()
     end = time.time()
-    for idx, (samples, targets) in enumerate(data_loader):
-        samples = samples.cuda(non_blocking=True)
-        targets = targets.cuda(non_blocking=True)
+    for idx, (samples, target_s1, target_s2, target_s3) in enumerate(data_loader):
+        samples = samples.cuda()
+        target_s1 = target_s1.cuda()
+        target_s2 = target_s2.cuda()
+        target_s3 = target_s3.cuda()
 
         with torch.cuda.amp.autocast(enabled=config.AMP_ENABLE):
             outputs = model(samples)
-        loss1 = criterion(outputs['stage1'], targets)
-        loss2 = criterion(outputs['stage2'], targets)
-        loss3 = criterion(outputs['stage3'], targets)
+        loss1 = criterion(outputs['stage1'], target_s1.float())
+        loss2 = criterion(outputs['stage2'], target_s2.float())
+        loss3 = criterion(outputs['stage3'], target_s3.float())
         loss = loss1 / config.TRAIN.ACCUMULATION_STEPS + \
             loss2 / config.TRAIN.ACCUMULATION_STEPS + \
             loss3 / config.TRAIN.ACCUMULATION_STEPS
@@ -225,7 +235,10 @@ def train_one_epoch(config, model, criterion, data_loader, optimizer, epoch, lr_
 
         torch.cuda.synchronize()
 
-        loss_meter.update(loss.item(), targets.size(0))
+        loss_meter.update(loss.item(), target_s1.size(0))
+        loss1_meter.update(loss1.item(), target_s1.size(0))
+        loss2_meter.update(loss2.item(), target_s1.size(0))
+        loss3_meter.update(loss3.item(), target_s1.size(0))
         scaler_meter.update(loss_scale_value)
         batch_time.update(time.time() - end)
         end = time.time()
@@ -240,42 +253,93 @@ def train_one_epoch(config, model, criterion, data_loader, optimizer, epoch, lr_
                 f'eta {datetime.timedelta(seconds=int(etas))} lr {lr:.6f}\t wd {wd:.4f}\t'
                 f'time {batch_time.val:.4f} ({batch_time.avg:.4f})\t'
                 f'loss {loss_meter.val:.4f} ({loss_meter.avg:.4f})\t'
+                f'loss1 {loss1_meter.val:.4f} ({loss1_meter.avg:.4f})\t'
+                f'loss2 {loss2_meter.val:.4f} ({loss2_meter.avg:.4f})\t'
+                f'loss3 {loss3_meter.val:.4f} ({loss3_meter.avg:.4f})\t'
                 f'loss_scale {scaler_meter.val:.4f} ({scaler_meter.avg:.4f})\t'
                 f'mem {memory_used:.0f}MB')
     epoch_time = time.time() - start
     logger.info(f"EPOCH {epoch} training takes {datetime.timedelta(seconds=int(epoch_time))}")
 
 
+def accuracy(output, target, threshold=0.5):
+    assert output.size() == target.size(), 'size of output must be the same as target'
+    preds = torch.sigmoid(output)
+    pred_labels = torch.where(output > threshold, 1, 0)
+
+    matching_elements = torch.eq(pred_labels, target)
+    is_one = torch.eq(target, 1)
+
+    correct = torch.logical_and(matching_elements, is_one).sum().item()
+    wrong = torch.logical_xor(pred_labels, target).sum().item()
+
+    with open('./output/validation/acc.txt', 'a') as f:
+        if is_one.sum() == 0 and pred_labels.sum() == 0:
+            f.write('acc: 100 %\n\n'
+                    f'{pred_labels=}\n\n'
+                    f'{target=}\n'
+                    '--------------------------------------------------------------\n\n')
+        else:
+            f.write(f'acc: {correct / (correct + wrong) * 100} %\n\n'
+                    f'{pred_labels=}\n\n'
+                    f'{target=}\n'
+                    '--------------------------------------------------------------\n\n')
+
+    if is_one.sum() == 0 and pred_labels.sum() == 0:
+        return 100.0
+    return correct / (correct + wrong) * 100
+
+
 @torch.no_grad()
 def validate(config, data_loader, model):
-    criterion = SmoothCrossEntropy()
+    # criterion = SmoothCrossEntropy()
+    criterion = nn.BCEWithLogitsLoss()
     model.eval()
 
     batch_time = AverageMeter()
     loss_meter = AverageMeter()
     acc1_meter = AverageMeter()
-    acc5_meter = AverageMeter()
+    acc2_meter = AverageMeter()
+    acc3_meter = AverageMeter()
 
     end = time.time()
-    for idx, (samples, targets) in enumerate(data_loader):
-        images = samples.cuda(non_blocking=True)
-        target = targets.cuda(non_blocking=True)
+    for idx, (samples, target_s1, target_s2, target_s3) in enumerate(data_loader):
+        images = samples.cuda()
+        target_s1 = target_s1.cuda()
+        target_s2 = target_s2.cuda()
+        target_s3 = target_s3.cuda()
 
         # compute output
         with torch.cuda.amp.autocast(enabled=config.AMP_ENABLE):
             outputs = model(images)
 
         # measure accuracy and record loss
-        loss = criterion(outputs, targets)
-        acc1, acc5 = accuracy(outputs, targets, topk=(1, 5))
+        loss1 = criterion(outputs['stage1'], target_s1.float())
+        loss2 = criterion(outputs['stage2'], target_s2.float())
+        loss3 = criterion(outputs['stage3'], target_s3.float())
+        loss = loss1 / config.TRAIN.ACCUMULATION_STEPS + \
+            loss2 / config.TRAIN.ACCUMULATION_STEPS + \
+            loss3 / config.TRAIN.ACCUMULATION_STEPS
 
-        acc1 = reduce_tensor(acc1)
-        acc5 = reduce_tensor(acc5)
-        loss = reduce_tensor(loss)
+        # acc1_stage1, acc5_stage1 = accuracy(outputs['stage1'], target_s1, topk=(1, 5))
+        # acc1_stage2, acc5_stage2 = accuracy(outputs['stage2'], target_s2, topk=(1, 5))
+        # acc1_stage3, acc5_stage3 = accuracy(outputs['stage3'], target_s3, topk=(1, 5))
 
-        loss_meter.update(loss.item(), target.size(0))
-        acc1_meter.update(acc1.item(), target.size(0))
-        acc5_meter.update(acc5.item(), target.size(0))
+        # acc1 = acc1_stage1 + acc1_stage2 + acc1_stage3
+        # acc5 = acc5_stage1 + acc5_stage2 + acc5_stage3
+
+        # acc1 = reduce_tensor(acc1)
+        # acc5 = reduce_tensor(acc5)
+        # loss = reduce_tensor(loss)
+
+        acc1 = accuracy(outputs['stage1'], target_s1, threshold=0.6)
+        acc2 = accuracy(outputs['stage2'], target_s2, threshold=0.6)
+        acc3 = accuracy(outputs['stage3'], target_s3, threshold=0.6)
+
+        loss_meter.update(loss.item(), target_s1.size(0))
+        acc1_meter.update(acc1, target_s1.size(0))
+        acc2_meter.update(acc2, target_s1.size(0))
+        acc3_meter.update(acc3, target_s1.size(0))
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -288,13 +352,16 @@ def validate(config, data_loader, model):
                 f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                 f'Loss {loss_meter.val:.4f} ({loss_meter.avg:.4f})\t'
                 f'Acc@1 {acc1_meter.val:.3f} ({acc1_meter.avg:.3f})\t'
-                f'Acc@5 {acc5_meter.val:.3f} ({acc5_meter.avg:.3f})\t'
+                f'Acc@2 {acc2_meter.val:.3f} ({acc2_meter.avg:.3f})\t'
+                f'Acc@3 {acc3_meter.val:.3f} ({acc3_meter.avg:.3f})\t'
                 f'Mem {memory_used:.0f}MB')
-    logger.info(f' * Acc@1 {acc1_meter.avg:.3f} Acc@5 {acc5_meter.avg:.3f}')
-    return acc1_meter.avg, acc5_meter.avg, loss_meter.avg
+    logger.info(f' * Acc@1 {acc1_meter.avg:.3f} Acc@2 {acc2_meter.avg:.3f} Acc@3 {acc3_meter.avg:.3f} ')
+    return acc1_meter.avg, acc2_meter.avg, acc2_meter.avg, loss_meter.avg
 
 
 if __name__ == "__main__":
+    # DOSと勘違いされないようにする
+    Image.MAX_IMAGE_PIXELS = 1000000000
     args, config = parse_option()
 
     seed = config.SEED
--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-PYTHONPATH="./swin-transformer/:$PYTHONPATH" python3 -m torch.distributed.launch main.py --cfg ./swin-transformer/configs/pst/swin_base_patch4_window12_352_finetune_patch_selection.yaml --local_rank 0 --output output/pst/default
+CUDA_LAUNCH_BLOCKING=1 PYTHONPATH="./swin-transformer/:$PYTHONPATH" python3 -m torch.distributed.launch main.py --cfg ./swin-transformer/configs/pst/swin_base_patch4_window12_352_finetune_patch_selection.yaml --local_rank 0 --output output/pst/default
